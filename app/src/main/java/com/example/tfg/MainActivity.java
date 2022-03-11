a463975db87f12b7c6b01f998c185da65a5cf787
Giovanni Alexander Cruz <giovannialexandercruzosegueda@gmail.com>
2022-03-11 15:55:33 +0100

    hacer que se muestren las contraseñas en java registro y MainActivity

--- app/src/main/java/com/example/tfg/MainActivity.java
+++ app/src/main/java/com/example/tfg/MainActivity.java
@@ -4,15 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean esVisible;
+    EditText password;
+    ImageView visible;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        visible=findViewById(R.id.ojo3);
+        password = findViewById(R.id.contraseña);
+        visible.setOnClickListener((View.OnClickListener) v -> {
+            if(!esVisible) {
+                password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                visible.setImageResource(R.drawable.ojo);
+                esVisible = true;
+            }
+            else {
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                esVisible = false;
+                visible.setImageResource(R.drawable.ojo_tachado);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/tfg/Registro.java
+++ app/src/main/java/com/example/tfg/Registro.java
@@ -1,14 +1,51 @@
 package com.example.tfg;
 
 import android.os.Bundle;
-
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Registro extends AppCompatActivity {
+    private boolean esVisible;
+    EditText cont1,cont2;
+    ImageView v1,v2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        v1 = findViewById(R.id.ojo);
+        v2 = findViewById(R.id.ojo1);
+        cont1 = findViewById(R.id.contraseña_reg);
+        cont2 = findViewById(R.id.confir_contraseña_reg);
+        v1.setOnClickListener((View.OnClickListener) v -> {
+            if(!esVisible) {
+                cont1.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                v1.setImageResource(R.drawable.ojo);
+                esVisible = true;
+            }
+            else {
+                cont1.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                esVisible = false;
+                v1.setImageResource(R.drawable.ojo_tachado);
+            }
+        });
+
+        v2.setOnClickListener((View.OnClickListener) v -> {
+            if(!esVisible) {
+                cont2.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                v2.setImageResource(R.drawable.ojo);
+                esVisible = true;
+            }
+            else {
+                cont2.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                v2.setImageResource(R.drawable.ojo_tachado);
+                esVisible = false;
+            }
+        });
+
     }
 
 
